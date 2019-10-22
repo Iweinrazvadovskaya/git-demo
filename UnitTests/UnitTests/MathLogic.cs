00b0ccb8e85ca6402fe0aabbc3209f4cf5c75293
Iweinrazvadovskaya <jane_rob@icloud.com>
2019-10-22 09:27:14 +0300

    cleanCode refactoring

--- UnitTests/UnitTests/MathLogic.cs
+++ UnitTests/UnitTests/MathLogic.cs
@@ -5,35 +5,17 @@ using System.Text;
 namespace UnitTests
 {
     class MathLogic
-    {/*            bool resultEx;
-            MathLogic traingle = new MathLogic();
-            traingle.a = 3;
-            traingle.b = 4;
-            traingle.c = 5;
-            resultEx = traingle.triangleCheck(traingle.a, traingle.b, traingle.c);*/
-        public double a { get; set; }
-        public double b { get; set; }
-        public double c { get; set; }
+    {
+        private double a { get; set; }
+        private double b { get; set; }
+        private double c { get; set; }
 
-        public bool triangleCheck(double first, double second, double third)
+        public bool TriangleCheck(double first, double second, double third)
         {
-            if (first > 0 || second > 0 || third > 0)
-            {
+           
                 return (first < second + third || second < first + third || third < first + second);
-            }
-            else { return false; }
                
         }
-        //public MathLogic(double a_, double b_, double c_)
-        //{
-        //    if (a_ > 0 && b_ > 0 && c_ > 0)
-        //    {
-        //        a = a_;
-        //        b = b_;
-        //        c = c_;
-        //    }
-            
-        //}
         static void Main(string[] args)
         {
             TestCaseNUnit test1 = new TestCaseNUnit();
--- UnitTests/UnitTests/TestCaseNUnit.cs
+++ UnitTests/UnitTests/TestCaseNUnit.cs
@@ -13,76 +13,76 @@ namespace UnitTests
 
         public  void Test_TraingleValues()
         {
-            Assert.AreEqual(true, traingle.triangleCheck(3, 4, 5));
+            Assert.AreEqual(true, traingle.TriangleCheck(3, 4, 5));
         }
 
         [TestCase]
 
         public void Test_NotTraingleValues()
         {
-            bool result = traingle.triangleCheck(1, 4, 5);
+            bool result = traingle.TriangleCheck(1, 4, 5);
             Assert.AreNotEqual(true, result);
         }
 
         [TestCase]
 
-        public void Test_ZiroValue()
+        public void Test_FirstValueIsZiro()
         {
-            bool result = traingle.triangleCheck(0, 4, 5);
+            bool result = traingle.TriangleCheck(0, 4, 5);
               Assert.IsFalse(result);
         }
 
         [TestCase]
 
-        public void Test_ZiroValue2()
+        public void Test_AllValueAreZiro()
         {
-            bool result = traingle.triangleCheck(0, 0, 0);
+            bool result = traingle.TriangleCheck(0, 0, 0);
             Assert.IsFalse(result);
         }
 
         [TestCase]
 
-        public void Test_NegativeValues()
+        public void Test_AllNegativeValues()
         {
-            bool result = traingle.triangleCheck(-2, -3, -5);
+            bool result = traingle.TriangleCheck(-2, -3, -5);
             Assert.IsFalse(result);
         }
 
-        public void Test_NegativeValues2()
+        public void Test_OneNegativeValues()
         {
-            bool result = traingle.triangleCheck(-1, 3, 5);
+            bool result = traingle.TriangleCheck(-1, 3, 5);
             Assert.IsFalse(result);
         }
 
         [TestCase]
 
-        public void Test_Values()
+        public void Test_RightValues()
         {
-            bool result = traingle.triangleCheck(10, 15, 15);
+            bool result = traingle.TriangleCheck(10, 15, 15);
             Assert.IsTrue(result);
         }
 
         [TestCase]
 
-        public void Test_Values2()
+        public void Test_RightUnequalValues()
         {
-            bool result = traingle.triangleCheck(5, 7, 9);
+            bool result = traingle.TriangleCheck(5, 7, 9);
             Assert.IsTrue(result);
         }
 
         [TestCase]
 
-        public void Test_Values3()
+        public void Test_RightEqualValues()
         {
-            bool result = traingle.triangleCheck(1, 2, 1);
+            bool result = traingle.TriangleCheck(1, 2, 1);
             Assert.IsFalse(result);
         }
 
         [TestCase]
 
-        public void Test_Values4()
+        public void Test_NotIntegerValue()
         {
-            bool result = traingle.triangleCheck(0.4, 4, 4);
+            bool result = traingle.TriangleCheck(0.4, 4, 4);
             Assert.IsTrue(result);
         }
     }
